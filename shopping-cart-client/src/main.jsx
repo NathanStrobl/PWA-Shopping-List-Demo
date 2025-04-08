3098a76dedacb8229afa8aad0efef65b3fe4eac0
NathanStrobl <111808514+NathanStrobl@users.noreply.github.com>
2025-04-08 19:57:17 -0400

    Website is now installable as a PWA.

--- shopping-cart-client/src/main.jsx
+++ shopping-cart-client/src/main.jsx
@@ -7,4 +7,8 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+if('serviceWorker' in navigator) {
+  window.addEventListener('load', () => navigator.serviceWorker.register('./serviceWorker.js'));
+}
