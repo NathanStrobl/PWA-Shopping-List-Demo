2d7434d7582505e37b482ec76bbc4d458448067a
NathanStrobl <111808514+NathanStrobl@users.noreply.github.com>
2025-04-08 23:52:42 -0400

    Should be good now.

--- shopping-cart-client/src/pages/Homepage.jsx
+++ shopping-cart-client/src/pages/Homepage.jsx
@@ -1,5 +1,10 @@
 import CategoryButton from "../components/CategoryButton";
-import WalmarksLogo from '../assets/walmarks.png';
+import AppleImage from '/images/apple.jpg';
+import GoogleHomeImage from '/images/googlehome.png';
+import PencilsImage from '/images/pencils.jpg';
+import ShirtImage from '/images/shirt.png';
+import SlinkyImage from '/images/slinky.jpg';
+import SofaImage from '/images/sofa.jpg';
 
 export default function Homepage() {
     return (
@@ -9,15 +14,12 @@ export default function Homepage() {
                 <h3>Select one of the categories below to begin browsing our selection.</h3>
             </div>
             <div className="categories-container">
-                <CategoryButton title="Clothing" image={WalmarksLogo} path='/browse?category=Test' />
-                <CategoryButton title="Home Goods" image={WalmarksLogo} />
-                <CategoryButton title="Electronics" image={WalmarksLogo} />
-                <CategoryButton title="Office Supplies" image={WalmarksLogo} />
-                <CategoryButton title="Groceries" image={WalmarksLogo} />
-                <CategoryButton title="Toys" image={WalmarksLogo} />
-                <CategoryButton title="Health" image={WalmarksLogo} />
-                <CategoryButton title="Hygiene" image={WalmarksLogo} />
-                <CategoryButton title="Automotive" image={WalmarksLogo} />
+                <CategoryButton title="Clothing" image={ShirtImage} path='/browse?category=Clothing' />
+                <CategoryButton title="Home Goods" image={SofaImage} path='browse?category=Home%20Goods' />
+                <CategoryButton title="Electronics" image={GoogleHomeImage} path='browse?category=Electronics' />
+                <CategoryButton title="Office Supplies" image={PencilsImage} path='browse?category=Office%20Supplies' />
+                <CategoryButton title="Groceries" image={AppleImage} path='browse?category=Groceries' />
+                <CategoryButton title="Toys" image={SlinkyImage} path='browse?category=Toys' />
             </div>
         </>
     );
